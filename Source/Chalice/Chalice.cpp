c3438dc8920b61af2ad6ba4400625b2531e70739
Spencer Melnick <smelnick97@gmail.com>
2020-11-18 15:52:36 -0500

    Adding starting project and map

---
+++ Source/Chalice/Chalice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Chalice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chalice, "Chalice" );
---
+++ Source/Chalice/ChaliceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaliceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHALICE_API AChaliceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
