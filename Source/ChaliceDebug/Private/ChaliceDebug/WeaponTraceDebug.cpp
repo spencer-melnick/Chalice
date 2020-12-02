fdb3bda3e2b9bab88633b8f73338021da223a168
Spencer Melnick <smelnick97@gmail.com>
2020-12-02 16:03:34 -0500

    Making weapon debug shapes brighter when trace is enabled

--- Source/ChaliceDebug/Private/ChaliceDebug/WeaponTraceDebug.cpp
+++ Source/ChaliceDebug/Private/ChaliceDebug/WeaponTraceDebug.cpp
@@ -28,23 +28,26 @@ void FWeaponTraceDebug::OnShowDebugInfo(AHUD* HUD, UCanvas* Canvas, const FDebug
 void FWeaponTraceDebug::DrawDebugInfo(UWeaponTraceComponent* TraceComponent, AHUD* HUD, UCanvas* Canvas, const FDebugDisplayInfo& DisplayInfo, float& YL, float& YPos)
 {
 	check(TraceComponent);
+	
+	const bool bTraceEnabled = TraceComponent->IsTraceEnabled();
+	const FColor ShapeColor = bTraceEnabled ? FColor(184, 224, 255) : FColor::Cyan;
+	
+    if (bTraceEnabled)
+    {
+        for (const FVector& HitLocation : TraceComponent->LastHitLocations)
+        {
+            DrawDebugPoint(HUD->GetWorld(), HitLocation, 10.f, FColor::Green, false, 0.3f);
+        }
+    }
 
 	for (const FWeaponTraceShape& TraceShape : TraceComponent->TraceShapes)
 	{
 		const FVector TraceLocation = TraceComponent->GetTraceShapeLocation(TraceShape);
-		DrawDebugSphere(HUD->GetWorld(), TraceLocation, TraceShape.Radius, 16, FColor::Cyan);
+		DrawDebugSphere(HUD->GetWorld(), TraceLocation, TraceShape.Radius, 16, ShapeColor);
 
-		if (TraceComponent->IsTraceEnabled())
+		if (bTraceEnabled)
 		{
 			DrawDebugLine(HUD->GetWorld(), TraceShape.PreviousPosition, TraceLocation, FColor::Red, false, 0.3f, 0, 1.f);
-		}
-	}
-
-	if (TraceComponent->IsTraceEnabled())
-	{
-		for (const FVector& HitLocation : TraceComponent->LastHitLocations)
-		{
-			DrawDebugPoint(HUD->GetWorld(), HitLocation, 10.f, FColor::Green);
 		}
 	}
 }
