7d6953935fe36de88c77fe38838e477d91fd89da
Redesigner <stephenwmelnick@gmail.com>
2020-12-16 14:04:41 -0500

    Added support for varying types of doors

---
+++ Source/ChaliceGame/Private/ChaliceGame/World/DoorHinged.cpp
@@ -0,0 +1,16 @@
+﻿#include "ChaliceGame/World/DoorHinged.h"
+
+ADoorHinged::ADoorHinged()
+{
+	return;
+}
+
+void ADoorHinged::Open()
+{
+	Super::Open();
+}
+
+void ADoorHinged::Close()
+{
+	Super::Close();
+}
