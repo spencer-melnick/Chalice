aa6b83d042490a4a95f297c88a1508afbd4dbbe7
Spencer Melnick <smelnick97@gmail.com>
2020-12-18 11:45:11 -0500

    Creating actor subsystem

---
+++ Source/ChaliceCore/Private/ChaliceCore/Subsystems/ActorSubsystem.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright (c) 2020 Spencer Melnick
+
+#include "ChaliceCore/Subsystems/ActorSubsystem.h"
+#include "EngineUtils.h"
+
+
+// UActorSubsystem
+
+// Subsystem overrides
+
+void UActorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	// Somewhat slow method for grabbing all the existing actors of this type, but it should be okay if
+	// only run on world startup
+	for (AActor* Actor : TActorRange<AActor>(GetWorld()))
+	{
+		if (Actor->GetClass()->ImplementsInterface(GetInterfaceType()))
+		{
+			RegisterActor(Actor);
+		}
+	}
+
+	ActorSpawnedDelegate.BindUObject(this, &UActorSubsystem::ActorSpawned);
+	ActorSpawnedDelegateHandle = GetWorld()->AddOnActorSpawnedHandler(ActorSpawnedDelegate);
+}
+
+void UActorSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	
+	for (AActor* Actor : Actors)
+	{
+		UnregisterActor(Actor);
+	}
+
+	GetWorld()->RemoveOnActorSpawnedHandler(ActorSpawnedDelegateHandle);
+}
+
+
+
+// Helpers
+
+void UActorSubsystem::RegisterActor(AActor* Actor)
+{
+	Actors.Add(Actor);
+	Actor->OnEndPlay.AddUniqueDynamic(this, &UActorSubsystem::ActorEndPlay);
+}
+
+void UActorSubsystem::UnregisterActor(AActor* Actor)
+{
+	Actors.Remove(Actor);
+
+	if (!Actor->IsPendingKill())
+	{
+		Actor->OnEndPlay.RemoveDynamic(this, &UActorSubsystem::ActorEndPlay);
+	}
+}
+
+
+// Callbacks
+
+void UActorSubsystem::ActorSpawned(AActor* Actor)
+{
+	if (Actor->GetClass()->ImplementsInterface(GetInterfaceType()))
+	{
+		RegisterActor(Actor);
+	}
+}
+
+void UActorSubsystem::ActorEndPlay(AActor* Actor, EEndPlayReason::Type Reason)
+{
+	UnregisterActor(Actor);
+}
+
---
+++ Source/ChaliceCore/Public/ChaliceCore/Subsystems/ActorSubsystem.h
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2020 Spencer Melnick
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "ActorSubsystem.generated.h"
+
+
+
+/**
+ * Subsystem that automatically tracks all actors implementing a specific interface
+ */
+UCLASS(Abstract)
+class CHALICECORE_API UActorSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	// Subsystem overrides
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+
+	// Actor subsystem interface
+
+	UFUNCTION(BlueprintCallable)
+	virtual UClass* GetInterfaceType() const { unimplemented(); return nullptr; }
+
+	UFUNCTION(BlueprintCallable)
+	const TArray<AActor*>& GetActors() const { return Actors; }
+
+
+protected:
+
+	// Helpers
+
+	void RegisterActor(AActor* Actor);
+	void UnregisterActor(AActor* Actor);
+
+
+private:
+
+	// Callbacks
+
+	virtual void ActorSpawned(AActor* Actor);
+
+	UFUNCTION()
+	virtual void ActorEndPlay(AActor* Actor, EEndPlayReason::Type Reason);
+
+
+	// Private properties
+
+	UPROPERTY()
+	TArray<AActor*> Actors;
+
+	FOnActorSpawned::FDelegate ActorSpawnedDelegate;
+	FDelegateHandle ActorSpawnedDelegateHandle;
+	
+};
